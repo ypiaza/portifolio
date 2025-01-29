7416fa43fad65722a8bae40fafa4238852a0e042
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-01-29 13:30:37 -0300

    Alguns ajustes na area de projetos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,30 @@
 import { Dados, projects } from '../data/data'
+import { useState } from 'react'
 
 const Projects = () => {
+
+    const [effectProject, setEffectProject] = useState<number | null>(null);
+
     return (
-        <div className="grid grid-cols-1 grid-rows-3 justify-center items-center h-full">
+        <div className="grid grid-cols-1 grid-rows-3 justify-center items-center h-full ">
+
             {Dados.map((item: projects, index: number) => (
-                <div className='flex flex-col p-4 '>
-                    <div>
-                        <h2 className='font-bold text-white'>{item.name}</h2>
+                <div 
+                key={index} 
+                className='grid grid-rows-2 p-4 cursor-pointer h-full relative border border-white/10 hover:border-white/50 hover:shadow-2xl hover:shadow-white/30 duration-300 overflow-hidden'
+                onMouseOver={() => setEffectProject(index)}
+                onMouseOut={() => setEffectProject(null)}
+                >
+
+                    <div className={`${effectProject === index ? 'absolute -translate-y-56 duration-300' : ''}`}>
+                        <h2 className={`font-bold text-white`}>{item.name}</h2>
                         <h3 className='font-semibold text-[0.80rem] text-white'>{item.tecnologies}</h3>
                     </div>
-                    <div>
-                        <img src={item.image} />
+
+                    <div className={`${effectProject === index ? '' : ''}`}>
+                        <img className='w-full' src={item.image} />
                     </div>
+
                 </div>
             ))}
         </div>
