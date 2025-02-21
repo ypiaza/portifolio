b01fb08ca794e31ee5b5dd5167469ef140a127db
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-02-21 13:23:51 -0300

    Update de código

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -8,7 +8,7 @@ const Skills = () => {
   return (
     <div className='w-full grid grid-cols-2 h-full text-white/70'>
       {skills.map((item: dataSkill, index: number) => (
-        <div className={`flex items-center justify-center border border-white/10 cursor-pointer hover:bg-[#${item.color}] duration-300  hover:border-white/20  hover:text-white hover:shadow-2xl hover:shadow-[#${item.color}] ${hoverSkill === index ? 'delay-700 duration-300' : ''}`}
+        <div className={`flex items-center justify-center border border-white/10 hover:bg-[#${item.color}] duration-300  hover:border-white/20  hover:text-white hover:shadow-2xl hover:shadow-[#${item.color}] ${hoverSkill === index ? 'delay-700 duration-300' : ''}`}
         onMouseOver={() => setHoverSkill(index)}
         onMouseOut={() => setHoverSkill(null)}
         >
